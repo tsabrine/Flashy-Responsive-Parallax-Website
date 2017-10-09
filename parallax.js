5272713b61d2346a91153925060a96959ad2c6d9
tsabrine <tizaouisabrine01@gmail.com>
2017-10-10 00:03:38 +0200

    initial commit: Parallax features in comment

---
+++ parallax.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+/*$(window).scroll(function(){
+	parallax();
+})
+
+function parallax() {
+	var wScroll = $(window).scrollTop();
+	$('.parallax--bg').css('background-position', 'center '+(wScroll*.75)+'px');
+	
+	$('.parallax-box').css('top', -5+(wScroll*.025)+'em');
+}*/
